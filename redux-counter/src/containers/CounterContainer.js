2f12af124e34901d43f52d2b0f7790a63eff41a7
ilhoon lee <rival6821@naver.com>
2018-11-01 19:49:44 +0900

    리듀서

---
+++ redux-counter/src/containers/CounterContainer.js
@@ -0,0 +1,50 @@
+import Counter from '../components/Counter';
+import * as actions from '../actions';
+import {connect} from 'react-redux';
+
+//랜덤 색상선택 함수
+export function getRandomColor(){
+    const colors = [
+        '#495057',
+        '#f03e3e',
+        '#d6336c',
+        '#ae3ec9',
+        '#7048e8',
+        '#4263eb',
+        '#1c7cd6',
+        '#1098ad',
+        '#0ca678',
+        '#37b24d',
+        '#74b816',
+        '#f59f00',
+        '#f76707'
+    ];
+
+    const random  = Math.floor(Math.random()*13);
+
+    return colors[random];
+}
+
+//state와 props연결
+
+const mapStateToProps = (state)=>({
+    color:state.color,
+    number:state.number
+});
+
+//dispatch를 파라미터로 받아 액션을 디스패치하는 함수들을 객체안에 넣어서 반환
+const mapDispatchToProps = (dispatch)=>({
+    onIncrement:()=>dispatch(actions.increment()),
+    onDevrement:()=>dispatch(actions.decrement()),
+    onSetColor:()=>{
+        const color = getRandomColor();
+        dispatch(actions.setColor(color));
+    }
+});
+
+const CounterContainer = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter);
+
+export default CounterContainer;
