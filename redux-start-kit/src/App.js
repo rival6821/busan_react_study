bc8d3aeea2dfdf92aa1a946ae9c724e3d2566d3b
ilhoon lee <rival6821@naver.com>
2018-11-10 15:22:49 +0900

    리덕스 미들웨어 작업중 axios

--- redux-start-kit/src/App.js
+++ redux-start-kit/src/App.js
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as counterActions from './modules/counter';
+import * as postActions from './modules/post';
 
 
 class App extends Component {
+    loadData=()=>{
+        const {PostActions, number} = this.props;
+        PostActions.getPost(number);
+    }
+    componentDidMount(){
+        this.loadData();
+    }
+    componentDidUpdate(prevProps,prevState){
+        if(this.props.number!==prevProps.number){
+            this.loadData();
+        }
+    }
     render() {
-        const { CounterActions, number } = this.props;
+        const { CounterActions, number, post, error, loading } = this.props;
 
         
         return (
             <div>
-                <h1>{number}</h1>
+                <h1>{number}</h1>{
+                    loading
+                        ? (<h2>로딩중...</h2>)
+                        : (
+                        error
+                            ?(<h2>오류발생!</h2>)
+                            :(<div>
+                                <h2>{post.title}</h2>
+                                <p>{post.body}</p>
+                            </div>)
+                        )
+                }
                 <button onClick={CounterActions.increment}>+</button>
                 <button onClick={CounterActions.decrement}>-</button>
             </div>
@@ -21,9 +45,13 @@ class App extends Component {
 
 export default connect(
     (state) => ({
-        number: state.counter
+        number: state.counter,
+        post: state.post.data,
+        loading: state.post.pending,
+        error: state.post.error
     }),
     (dispatch) => ({
-        CounterActions: bindActionCreators(counterActions, dispatch)
+        CounterActions: bindActionCreators(counterActions, dispatch),
+        PostActions : bindActionCreators(postActions, dispatch)
     })
 )(App);
