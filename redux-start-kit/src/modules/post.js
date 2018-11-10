c6c0203a94832251959b9a2f159fd1d4c1a98d55
ilhoon lee <rival6821@naver.com>
2018-11-10 22:57:26 +0900

    리액트 라우터 작업중 370p

--- redux-start-kit/src/modules/post.js
+++ redux-start-kit/src/modules/post.js
@@ -1,63 +1,47 @@
 import {handleActions, createAction} from 'redux-actions';
+import { applyPenders } from 'redux-pender';
 import axios from 'axios';
 
-function getPostAPI(postId){
-    return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+function getPostAPI(postId) {
+  return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
 }
 
-const GET_POST_PENDING = 'GET_POST_PENDING';
-const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
-const GET_POST_FAILURE = 'GET_POST_FAILURE';
+const GET_POST = 'GET_POST';
 
-const getPostPending = createAction(GET_POST_PENDING);
-const getPostSuccess = createAction(GET_POST_SUCCESS);
-const getPostFailure = createAction(GET_POST_FAILURE);
-
-export const getPost = (postId)=>dispatch=>{
-    dispatch(getPostPending());
-
-    return getPostAPI(postId).then((response)=>{
-        dispatch(getPostSuccess(response));
-        return response;
-    }).catch(error=>{
-        dispatch(getPostFailure(error));
-        throw(error);
-    })
-}
+export const getPost = createAction(GET_POST, getPostAPI);
 
 const initialState = {
-    pending:false,
-    error:false,
-    data:{
-        title:'',
-        body:''
-    }
+  data: {
+    title: '',
+    body: ''
+  }
 }
 
-export default handleActions({
-    [GET_POST_PENDING]:(state,action)=>{
-        return {
-            ...state,
-            pending:true,
-            error:false
-        };
+const reducer = handleActions({
+    
+}, initialState);
+
+export default applyPenders(reducer, [
+  {
+    type: GET_POST, 
+    onSuccess: (state, action) => { 
+      const { title, body } = action.payload.data;
+      return {
+        data: {
+          title, 
+          body
+        }
+      }
     },
-    [GET_POST_SUCCESS]:(state,action)=>{
-        const {title,body} = action.payload.data;
-        return {
-            ...state,
-            pending:false,
-            data:{
-                title,
-                body
-            }
-        };
-    },
-    [GET_POST_FAILURE]:(state,action)=>{
-        return {
-            ...state,
-            pending:false,
-            error:true
-        };
+    onCancel: (state, action) => {
+      return {
+        data: {
+          title: '취소됨',
+          body: '취소됨'
+        }
+      }
     }
-},initialState);
+
+  },
+
+]);
