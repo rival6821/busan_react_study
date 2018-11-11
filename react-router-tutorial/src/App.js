0e4a2e269eb19c4b99f3d983e0a3e7f46a94b6cc
ilhoon lee <rival6821@naver.com>
2018-11-11 21:41:34 +0900

    16장 라우팅 완료

--- react-router-tutorial/src/App.js
+++ react-router-tutorial/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Route} from 'react-router-dom';
 
-import{Home,About} from 'pages';
+import{Home,About,Posts} from 'pages';
+import Menu from 'components/Menu';
 
 const App = () =>{
     return(
         <div>
+            <Menu/>
             <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
+            <Route path="/about/:name?" component={About}/>
+            <Route path="/posts" component={Posts}/>
         </div>
     )
 }
