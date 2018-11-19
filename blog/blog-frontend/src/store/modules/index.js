629b405b8269ae242be2efe107c064d75886873c
ilhoon lee <rival6821@naver.com>
2018-11-19 22:07:54 +0900

    20장 492페이지 컴포넌트 설정

---
+++ blog/blog-frontend/src/store/modules/index.js
@@ -0,0 +1,7 @@
+import { default as editor} from './editor';
+import { default as base} from './base';
+import { default as list} from './list';
+import { default as post} from './post';
+
+//  비동기 액션관리
+export { penderReducer as pender } from 'redux-pender';
