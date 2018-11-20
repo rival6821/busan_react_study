e31e997d9dacf9692858aae99407d87dedd2a3a4
ilhoon lee <rival6821@naver.com>
2018-11-20 22:43:12 +0900

    create-react-app 버전 다운그레이드

--- blog/blog-frontend/src/store/modules/editor.js
+++
@@ -1,9 +0,0 @@
-import { createAction, handleActions } from 'redux-actions';
-import { Map } from 'immutable';
-import { pender } from 'redux-pender';
-
-const initialState = Map({});
-
-export default handleActions({
-
-}, initialState);
